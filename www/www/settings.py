88333ccfa458f95a5050d292868a5ed3bd93ba8e
Oleg Kostin <kov@lins.ru>
2024-02-19 23:21:42 +0300

    static

--- www/www/settings.py
+++ www/www/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-w%wl6)_c0i4%e_l7ny^3*xbf21u(dcx%ke(0$ghjytyptfl8^-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -112,6 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# STATIC_ROOT = BASE_DIR / 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
